f118b3a7e7f42eb2b58b7cd1a8a1ea87dc745e1e
Daniel Chen <chendaniely@gmail.com>
2021-02-10 20:44:17 -0500

    put in intro section to word script by removing yaml header

--- word_export/combine_and_render.sh
+++ word_export/combine_and_render.sh
@@ -1,5 +1,10 @@
 cat word_export/yaml.yaml > word_export/doc.Rmd
+
+# remove yaml header
+# https://stackoverflow.com/questions/28221779/how-to-remove-yaml-frontmatter-from-markdown-files/29292490
+sed '1 { /^---/ { :a N; /\n---/! ba; d} }' index.Rmd >> word_export/doc.Rmd
 cat 10-*.Rmd >> word_export/doc.Rmd
+
 cat 20-*.Rmd >> word_export/doc.Rmd
 cat 30-*.Rmd >> word_export/doc.Rmd
 
